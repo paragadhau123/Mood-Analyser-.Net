66e4de198023f5790d21fd45523b4cf398787a69
Parag Adhau <adhauparag64@gmail.com>
2020-09-19 00:05:28 +0530

    Added :Test case to check when mood not sad

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -13,12 +13,19 @@ namespace MoodAnalyserTest
         }
 
         [Test]
-        public void WhenGivenSadMessageShouldReturnSad()
+        public void givenMessgae_WhenSad_ShouldReturn_Sad()
         {
             MoodAnalyserMain moodAnalyzer = new MoodAnalyserMain();
             String mood = moodAnalyzer.analyseMood("This is a Sad Message");
             Assert.AreEqual("SAD", mood);
-            }
-            
+        }
+
+        [Test]
+        public void givenMessage_WhenNotSad_ShouldReturn_Happy()
+        {
+            MoodAnalyserMain moodAnalyzer = new MoodAnalyserMain();
+            String mood = moodAnalyzer.analyseMood("This is a Happy Message");
+            Assert.AreEqual("HAPPY", mood);
         }
     }
+}
